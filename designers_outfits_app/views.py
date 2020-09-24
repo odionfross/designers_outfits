693e212be1eae0f1409a4bfb0574b71dafc2536e
Nathalie raob <raobimanana@gmail.com>
2020-09-23 21:43:24 -0400

    index-fashion.html commit

--- designers_outfits_app/views.py
+++ designers_outfits_app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     # retrieve info for the home page
-    return render(request, 'index.html')
+    return render(request, 'index-fashion.html')
+
+def NoResult(request):
+    return render(request, '404.html')
