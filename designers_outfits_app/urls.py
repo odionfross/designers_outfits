19af17a36ff4d25c666f8225e7db6865478f174a
dtran-fullstack <duytran0214@gmail.com>
2020-09-26 18:28:21 -0700

    Add API for front page

--- designers_outfits_app/urls.py
+++ designers_outfits_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
 	path('', views.index),
-	path('NoResult', views.NoResult)
+	path('NoResult', views.NoResult),
+	path('box',views.index_boxed),
+	path('classic',views.index_classic),
+	path('fashion',views.index_fashion),
+	path('landing',views.index_landing),
 ]
--- designers_outfits_app/views.py
+++ designers_outfits_app/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 def index(request):
     # retrieve info for the home page
-    return render(request, 'index-fashion.html')
+    return render(request, 'index.html')
 
 def NoResult(request):
     return render(request, '404.html')
+
+def index_boxed(request):
+    return render(request,'index-boxed.html')
+
+def index_classic(request):
+    return render(request,'index-classic.html')
+
+def index_fashion(request):
+    return render(request,'index-fashion.html')
+
+def index_landing(request):
+    return render(request,'index-landing.html')
