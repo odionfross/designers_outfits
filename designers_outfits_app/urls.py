2d461a925e06ab21ed0dcc5d9d2c210459e300dc
dtran-fullstack <duytran0214@gmail.com>
2020-10-01 15:15:11 -0600

    add API on product page and add more feature on fashion page

--- designers_outfits_app/urls.py
+++ designers_outfits_app/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index),
-	path('NoResult', views.NoResult),
-	path('box',views.index_boxed),
-	path('classic',views.index_classic),
+	path('not_found', views.not_found),
+	path('blog',views.blog_landing),
 	path('fashion',views.index_fashion),
 	path('landing',views.index_landing),
+	path('product/<int:id>',views.product),
 ]
--- designers_outfits_app/views.py
+++ designers_outfits_app/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
+import requests
 
-def index(request):
-    # retrieve info for the home page
-    return render(request, 'index.html')
-
-def NoResult(request):
+def not_found(request):
     return render(request, '404.html')
 
-def index_boxed(request):
-    return render(request,'index-boxed.html')
+def blog_landing(request):
+    return render(request,'blog_landing.html')
 
-def index_classic(request):
-    return render(request,'index-classic.html')
+def product(request,id):
+    url = "https://asos2.p.rapidapi.com/products/v3/detail"
+    querystring = {"store":"US","sizeSchema":"US","lang":"en-US","currency":"USD","id":id}
+    headers = {
+    'x-rapidapi-host': "asos2.p.rapidapi.com",
+    'x-rapidapi-key': "47e36b370fmsh4437be8672c0154p1c0cb8jsn42ab70fa5204"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    images = []
+    for i in response.json()['media']['images']:
+        images.append(i['url'])
+    context = {
+        "name" : response.json()['name'],
+        "des" : response.json()['description'],
+        "brand" : response.json()['brand']['name'],
+        "sizeGuide" : response.json()['sizeGuide'],
+        "variants" : response.json()['variants'],
+        "images" : images,
+        "info" : response.json()['info'],
+        "baseURL" : response.json()['baseUrl'],
+    }
+    return render(request,'product.html',context)
+
+# def product(request,id):
+#     return render(request,'product.html')
 
 def index_fashion(request):
     return render(request,'index-fashion.html')
 
 def index_landing(request):
     return render(request,'index-landing.html')
+
